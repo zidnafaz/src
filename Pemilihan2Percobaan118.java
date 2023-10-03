35b8c00f4b20fcf57a1c26e06e0f2b16eaccb99e
zidnafaz <zidnafaza27@gmail.com>
2023-10-03 14:44:03 +0700

    Pertanyaan 1 (penambahan code untuk 2100)

---
+++ Pemilihan2Percobaan118.java
@@ -0,0 +1,26 @@
+package src;
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan118 {
+    public static void main(String[] args) {
+
+        Scanner input18 = new Scanner(System.in);
+        
+        int
+        Tahun;
+
+        System.out.print("Masukkan Tahun      : ");
+        Tahun = input18.nextInt();
+
+        if ((Tahun % 4) == 0) {
+            if ((Tahun % 100) != 0)
+                System.out.println("Tahun Kabisat");
+            else if ((Tahun % 4) == 0)
+                System.out.println("Bukan Tahun Kabisat");
+        } else {
+            System.out.println("Bukan Tahun Kabisat");
+        }
+
+    }
+    
+}
